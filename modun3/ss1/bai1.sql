611b8c21a3b291c88d1a388bd4a2304a83328f68
dangphapqt1997 <dangphapqt1997@gmail.com>
2023-12-13 17:20:03 +0700

    hahahaah

--- modun3/ss1/bai1.sql
+++ modun3/ss1/bai1.sql
@@ -1,14 +1,14 @@
 create database student_management;
 use student_management;
 create table Class(
-id_c int primary key,
-name_c varchar(50)
+id_class int primary key,
+name_class varchar(50)
 );
 create table Teacher (
-id int,
+id_teacher int,
 name_teacher varchar(200),
 age int,
 country varchar(50),
-primary key(id)
+primary key(id_teacher)
 );
 
